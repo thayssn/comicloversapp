c3c3ef9e407bb0d4f30951f736e6b24a8a3e0007
Thays dos Santos <thays.sn@hotmail.com>
2019-06-13 22:08:00 -0300

    Alterando estado inicial dos reducers de livro e removendo reducer de teste

--- app/redux/reducers/text.js
+++
@@ -1,9 +0,0 @@
-const INITIAL_STATE = `I'm not Okay`
-export default function text(state = INITIAL_STATE, action){
-  switch(action.type){
-    case 'CHANGE_TEXT':
-      return action.text
-    default:
-      return state;
-  }
-}
