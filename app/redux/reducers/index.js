a31c86908acc41b1cb5a1c8d6ba63e56b5df0512
Thays dos Santos <thays.sn@hotmail.com>
2019-06-13 22:38:37 -0300

    Removendo importação do text nos reducers

--- app/redux/reducers/index.js
+++ app/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import text from './text';
 import collections from './collections';
 import books from './books';
 
 export default combineReducers({
-  text,
   collections,
   books
 })
