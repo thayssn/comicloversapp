aa1352937214c09222c7354fb80e90f7b86f6a92
Thays <thays.sn@hotmail.com>
2019-07-20 11:12:34 -0300

    Menu layout

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import {
-  ScrollView, View, StyleSheet, Text,
+  View, StyleSheet, Text,
 } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+import CLGradient from '../CLGradient';
+
 import { onSignOut } from '../../services/auth';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  innerContainer: {
+    flex: 1,
+    flexGrow: 1,
+    alignContent: 'space-between',
+    justifyContent: 'space-between',
+  },
   routes: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: '400',
     padding: 10,
+    color: '#FFF',
   },
   separator: {
     marginVertical: 20,
     height: 1,
     width: '100%',
-    backgroundColor: '#CCC',
+    backgroundColor: '#EEE',
+  },
+  navigation: {
+    flex: 1,
+    flexGrow: 1,
+
+  },
+  bottom: {
+    paddingBottom: 30,
   },
   logout: {
-    fontSize: 18,
-    fontWeight: '600',
-    padding: 10,
+    fontSize: 16,
+    fontWeight: '400',
+    paddingHorizontal: 10,
+    color: '#FFF',
   },
 });
 
@@ -31,29 +49,34 @@ const Menu = ({ navigation }) => {
   const { navigate, state } = navigation;
   const { routes } = state;
   return (
-    <ScrollView>
-      <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
-        { routes.map(({ routeName }) => (
+    <View style={styles.container}>
+      <CLGradient />
+      <SafeAreaView style={styles.innerContainer} forceInset={{ top: 'always', horizontal: 'never' }}>
+        <View style={styles.navigation}>
+          { routes.map(({ routeName }) => (
+            <Text
+              style={styles.routes}
+              key={routeName}
+              onPress={() => { navigate(routeName); }}
+            >
+              {routeName}
+            </Text>
+          ))}
+        </View>
+        <View style={styles.bottom}>
+          <View style={styles.separator} />
           <Text
-            style={styles.routes}
-            key={routeName}
-            onPress={() => { navigate(routeName); }}
+            style={styles.logout}
+            onPress={() => {
+              onSignOut();
+              navigate('Root');
+            }}
           >
-            {routeName}
+              Logout
           </Text>
-        ))}
-        <View style={styles.separator} />
-        <Text
-          style={styles.logout}
-          onPress={() => {
-            onSignOut();
-            navigate('Root');
-          }}
-        >
-          Logout
-        </Text>
+        </View>
       </SafeAreaView>
-    </ScrollView>
+    </View>
   );
 };
 
